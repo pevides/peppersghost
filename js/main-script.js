c85f9ace5e7a9b397ba5025f6446e787aa76eed6
Baltazar Reis <baltazar9reis@gmail.com>
2026-05-29 00:19:47 +0100

    Experimentar com distância da câmara a 30 para facilitar visualização no peppers ghost

--- js/main-script.js
+++ js/main-script.js
@@ -95,7 +95,7 @@ function createScene() {
 function createCamera() {
     'use strict';
     camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 0, 16);
+    camera.position.set(0, 0, 24);
     camera.lookAt(0, 0, 0);
 }
 
